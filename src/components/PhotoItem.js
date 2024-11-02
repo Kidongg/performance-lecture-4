61e6163de2d07df11d9d2ddf66d9f34f5d32279d
Kidongg <dlrlehd7@daum.net>
2024-11-02 20:44:23 +0900

    feat: 레이아웃 이동 피하기

--- src/components/PhotoItem.js
+++ src/components/PhotoItem.js
@@ -2,7 +2,7 @@ import React from 'react';
 import styled from 'styled-components';
 import { useDispatch } from 'react-redux';
 import { showModal } from '../redux/imageModal';
-
+ 
 function PhotoItem({ photo: { urls, alt } }) {
   const dispatch = useDispatch();
 
@@ -17,11 +17,19 @@ function PhotoItem({ photo: { urls, alt } }) {
   );
 }
 
-const ImageWrap = styled.div``;
+const ImageWrap = styled.div`
+  position: relative;
+  width: 100%;
+  padding-bottom: 56.25%;
+`;
 
 const Image = styled.img`
   cursor: pointer;
   width: 100%;
+  position: absolute;
+  height: 100%;
+  top: 0;
+  left: 0;
 `;
 
 export default PhotoItem;
